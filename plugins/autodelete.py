fde94204f2a2315972bb4acfa369946fe7c3fd11
Fasalul Rabbani <100061895+StuartLittleKL@users.noreply.github.com>
2022-04-02 05:52:31 +0530

    Update autodelete.py

--- plugins/autodelete.py
+++ plugins/autodelete.py
@@ -2,3 +2,25 @@ import asyncio
 from os import environ
 from pyrogram import Client, filters, idle
 Import json
+
+
+
+@User.on_message(filters.chat(GROUPS))
+async def delete(user, message):
+    try:
+       if message.from_user.id in ADMINS:
+          return
+       else:
+          await asyncio.sleep(TIME)
+          await Bot.delete_messages(message.chat.id, message.message_id)
+    except Exception as e:
+       print(e)
+       
+User.start()
+print("User Started!")
+
+idle()
+
+
+User.stop()
+print("User Stopped!")
